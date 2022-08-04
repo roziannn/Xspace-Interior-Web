a70eb936075c8aae6cae2bf1a70e63d2152e662b
Firda Rosiana T <firdaroziana10@gmail.com>
2022-08-04 13:17:51 +0700

    gallery product

--- app/Http/Controllers/ProductGalleryController.php
+++ app/Http/Controllers/ProductGalleryController.php
@@ -119,8 +119,10 @@ class ProductGalleryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(ProductGallery $gallery)
     {
-        //
+        $gallery->delete();
+
+        return redirect()->route('dashboard.product.gallery.index', $gallery->products_id);
     }
 }
