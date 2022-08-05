487e3a15b257941dd665d2f1ba0bf3f272f46ea2
Firda Rosiana T <firdaroziana10@gmail.com>
2022-08-05 14:28:32 +0700

    new update CRUD transaction

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductGalleryController;
+use App\Http\Controllers\TransactionController;
 use App\Models\User;
 use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,9 @@ Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('das
         Route::resource('product', ProductController::class);
         Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
             'index', 'create', 'store', 'destroy'
+        ]);
+        Route::resource('transaction', TransactionController::class)->only([
+            'index', 'show', 'edit', 'update'
         ]);
     });
 });
