a378df65fc8623ea4fa752d8438bcd4afb44ad1d
Firda Rosiana T <firdaroziana10@gmail.com>
2022-08-06 09:01:25 +0700

    update ada page cart with delete option

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FrontendController extends Controller
 {
@@ -18,11 +20,30 @@ class FrontendController extends Controller
     public function details(Request $request, $slug){
 
         $product = Product::with(['galleries'])->where('slug', $slug)->firstOrFail();
-        return view('pages.frontend.details', compact('product'));
+        $recommendations = Product::with(['galleries'])->inRandomOrder()->limit(4)->get();
+        
+        return view('pages.frontend.details', compact('product', 'recommendations'));
     }
 
+    public function cartAdd(Request $request, $id){
+        Cart::create([
+            'users_id' => Auth::user()->id,
+            'products_id' => $id
+        ]);
+
+        return redirect('cart');
+    }
+
+    public function cartDelete(Request $request, $id){
+        $item = Cart::findOrfail($id);
+
+        $item->delete();
+
+        return redirect('cart');
+    }
     public function cart(Request $request){
-        return view('pages.frontend.cart');
+        $carts = Cart::with(['product.galleries'])->where('users_id', Auth::user()->id)->get();
+        return view('pages.frontend.cart', compact('carts'));
     }
 
     public function success(Request $request){
